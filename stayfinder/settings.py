2144d4e5aeb823b040b46d69f245617a3a689181
Anandu kk <116473698+kkanandu@users.noreply.github.com>
2025-10-15 18:06:37 +0530

    Update settings.py

--- stayfinder/settings.py
+++ stayfinder/settings.py
@@ -107,6 +107,11 @@ WSGI_APPLICATION = 'stayfinder.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.dummy',
+    }
+}
 
 
 
