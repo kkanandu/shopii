08175298cb55980c002f9ae9505e63e13c86e1f1
Anandu kk <116473698+kkanandu@users.noreply.github.com>
2025-10-15 18:04:01 +0530

    Update settings.py

--- stayfinder/settings.py
+++ stayfinder/settings.py
@@ -108,12 +108,7 @@ WSGI_APPLICATION = 'stayfinder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
