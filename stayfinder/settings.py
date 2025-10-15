84329caa3c5c111591afe06ae40a0f46e79116b3
Anandu kk <116473698+kkanandu@users.noreply.github.com>
2025-10-15 18:00:28 +0530

    Update settings.py

--- stayfinder/settings.py
+++ stayfinder/settings.py
@@ -108,20 +108,10 @@ WSGI_APPLICATION = 'stayfinder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shopivex',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
